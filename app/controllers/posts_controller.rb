2d8751dcdc34c16d0216ec8ae82fd19a8423d5dc
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-28 19:51:43 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(post_params)
+	  @post = Post.new(post_params(:title,:description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
 	def update
 	  @post = Post.find(params[:id])
 	  
-	  @post.update(post_params)
+	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 	
